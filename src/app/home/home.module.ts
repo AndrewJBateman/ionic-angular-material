7fe31889e414cf1bc8d64240ac251c5ff7bdd16c
Andrew Bateman <gomezbateman@yahoo.com>
2019-06-11 22:03:46 +0200

    first commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import { MaterialModule } from '../material.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+		MaterialModule,
+    ReactiveFormsModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
